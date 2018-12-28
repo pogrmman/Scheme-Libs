5171d68a857ab3e61e097ec7c2c635c5c12aa76e
Alexander Mohn <pogrmman@gmail.com>
2018-12-28 09:45:09 -0600

    Initial working version

--- windows/srfi/compile-larceny.scm
+++ windows/srfi/compile-larceny.scm
@@ -1,19 +1,24 @@
 (import (scheme base)
         (scheme read)
+				(scheme write)
         (scheme file)
         (srfi 14)
         (srfi 115)
         (windows srfi 138))
 
 (define (read-commands file)
-  (call-with-input-file
-      file
-    (lambda (p)
-      (let kernel ((contents '())
-                   (line (read-line p)))
-        (if (eof-object? line)
-            (reverse contents)
-            (kernel (cons line contents) (read-line p)))))))
+	(begin
+		(display "Found config file: ")
+		(display file)
+		(newline)
+		(call-with-input-file
+				file
+			(lambda (p)
+				(let kernel ((contents '())
+										 (line (read-line p)))
+					(if (eof-object? line)
+							(reverse contents)
+							(kernel (cons line contents) (read-line p))))))))
 
 (define (parse-file-contents file-contents)
   ; Build the charsets we'll use to parse the file
@@ -24,11 +29,13 @@
          (path-chars (char-set-adjoin filename-chars #\space)))
     ; Set up regexes to grab the necessary information from the file
     (let* ((input-regex (regexp `(: "-pgm" #\space ($ (+ ,filename-chars) ".scm"))))
-           (output-regex (regexp `(: "-o" #\space ($ (+ ,filename-chars)) (? ".exe"))))
+           (output-regex (regexp `(: "-o" #\space ($ (+ ,filename-chars)) ".exe")))
            (path `($ (: ,char-set:letter ":\\" (* (+ ,path-chars) (? #\\)))))
            (libs `(: ,path (* (: #\; ,path))))
            (before-libs-regex (regexp `(: "-I" #\space ,libs)))
            (after-libs-regex (regexp `(: "-A" #\space ,libs))))
+			(display "Built regexes")
+			(newline)
       ; Look through the file contents for each regex (should I use map here?)
       (let ((input-file
              (find-in-list file-contents input-regex))
@@ -38,6 +45,8 @@
              (find-in-list file-contents before-libs-regex))
             (after-libs
              (find-in-list file-contents after-libs-regex)))
+				(display "Parsed file -- starting compilation")
+				(newline)
         ; Basic error handling
         (cond ((null? input-file)
                (error "parse-file-contents: please specify a file to compile"))
@@ -50,14 +59,25 @@
 
 ;; Start compilation
 (define (compile input-file output-file before-libs after-libs)
-  (let ((input-file (car (matches->lists input-file)))
+  (let ((input-file (caar (matches->lists input-file)))
         (output-file (if (not (null? output-file))
-                         (car (matches->lists output-file))
+                         (caar (matches->lists output-file))
                          #f))
         (before-libs (if (not (null? before-libs))
-                         (add-quotes (flatten (matches->lists before-libs)))))
+                         (add-quotes (flatten (matches->lists before-libs)))
+												 '()))
         (after-libs (if (not (null? after-libs))
-                        (add-quotes (flatten (matches->lists after-libs))))))
+                        (add-quotes (flatten (matches->lists after-libs)))
+												'())))
+		(display "Calling compile-r7rs ")
+		(display input-file)
+		(display " ")
+		(display output-file)
+		(display " ")
+		(display before-libs)
+		(display " ")
+		(display after-libs)
+		(newline)
     (compile-r7rs input-file output-file before-libs after-libs)))
 
 ;; List Helper Functions
@@ -102,3 +122,5 @@
   (let* ((matching (lambda (str) (regexp-matches regex str)))
          (result (map matching list)))
     (purge-false result)))
+
+(parse-file-contents (read-commands "compile.config"))
